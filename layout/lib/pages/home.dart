8413a34576808835eaeead99e45a329d889e97a5
zawanee <89334887+zawaneemakeng@users.noreply.github.com>
2023-02-20 21:29:04 +0700

    Update home.dart

--- layout/lib/pages/home.dart
+++ layout/lib/pages/home.dart
@@ -18,36 +18,63 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListView(children: [
-          MyBox(),
+          MyBox(
+              'บริติช ช็อตแฮร์ (British Shorthair)',
+              'British shorthair เป็นหนึ่งในสายพันธุ์แมวที่เก่าแก่ที่สุดของประเทศอังกฤษ',
+              'https://cdn.pixabay.com/photo/2022/01/29/10/59/cat-6977088_960_720.jpg'),
           const SizedBox(
-            height: 30,
+            height: 25,
           ),
-          MyBox(),
+          MyBox(
+              'สก๊อตทิช โฟลด์ (Scottish fold)',
+              'แมวมันช์กิ้น เป็นแมวสายพันธุ์ขนาดกลาง ขาสั้นกุดและลำตัวยาว มีคนเปรียบเปรยไว้ว่า แมวสายพันธุ์นี้เคลื่อนที่เหมือนตัวเฟร์ริต',
+              'https://cdn.pixabay.com/photo/2019/04/17/18/58/scottish-fold-4135062_960_720.jpg'),
           const SizedBox(
-            height: 30,
+            height: 25,
           ),
-          MyBox()
+          MyBox(
+              'แมววิเชียรมาศ (Siamese cat)',
+              'แมวพันธุ์วิเชียรมาศ หรือ แมวสยาม (Siamese cat) ที่เป็นดั่งตัวแทนสายพันธุ์แมวแห่งประเทศไทย ที่นอกจากจะมีเอกลักษณ์จากลวดลายและสีขนบนตัวอันงดงามแล้ว ยังมีลักษณะนิสัยที่เป็นมิตร ขี้อ้อน',
+              'https://cdn.pixabay.com/photo/2020/06/20/11/08/siamese-cat-5320568_960_720.jpg')
         ]),
       ),
     );
   }
 
-  Widget MyBox() {
+  Widget MyBox(String title, String detail, String imageUrl) {
     return Container(
       padding: const EdgeInsets.all(20),
-      color: const Color.fromARGB(255, 152, 205, 186),
-      height: 120,
+      //color: const Color.fromARGB(255, 152, 205, 186),
+      height: 160,
+      decoration: BoxDecoration(
+          //color: const Color.fromARGB(255, 152, 205, 186),
+          image: DecorationImage(
+            image: NetworkImage(imageUrl),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.25),
+              BlendMode.darken,
+            ),
+          ),
+          borderRadius: BorderRadius.circular(15)),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
+        children: [
           Text(
-            'บริติช ช็อตแฮร์ (British Shorthair)',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            title,
+            style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 227, 227, 227)),
+          ),
+          const SizedBox(
+            height: 5,
           ),
           Text(
-            ' British shorthair เป็นหนึ่งในสายพันธุ์แมวที่เก่าแก่ที่สุดของประเทศอังกฤษ',
-            style: TextStyle(fontSize: 14),
+            detail,
+            style: const TextStyle(
+                fontSize: 14, color: Color.fromARGB(255, 227, 227, 227)),
           ),
         ],
       ),
